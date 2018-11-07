a8c2915682fbf3ee7fc421fb64d4a2b633041d97
Luke Parker <lukep@digispend.io>
2018-11-07 15:41:06 -0600

    Start to prepare for threead and add get the Verifications.
    - Made global variables global, not in main.
    
    - Added locks.

--- src/main.nim
+++ src/main.nim
@@ -29,60 +29,101 @@ import EmberRPC
 #OS standard lib.
 import os
 
+#Locks standard lib.
+import locks
+
 #Async standard lib.
 import asyncdispatch
 
 #JSON standard lib.
 import json
 
-#Main function is so these variables can be GC'd.
+const
+    gensis: string = "EMB_DEVELOPER_TESTNET"
+
+var
+    #Connect to the EMB Node.
+    rpc: EmberRPC = waitFor newEmberRPC()
+    #Lock for using the RPC.
+    rpcLock: Lock
+    #Boolean for making sure async procs don't use the RPC at the same time.
+    rpcBool: bool
+    #Public Key to mine to.
+    publicKey: BLSPublicKey
+    #Difficulty.
+    difficulty: BN
+    #Nonce.
+    nonce: uint = uint(waitFor rpc.merit.getHeight())
+    #Last Block hash.
+    last: ArgonHash = (
+        waitFor rpc.merit.getBlock(
+            int(nonce - 1)
+        )
+    )["argon"].getStr().toArgonHash()
+    #Verifications object.
+    verifs: Verifications = newVerificationsObj()
+    #Miners object.
+    miners: Miners
+
+#Calculate the Verifications' signature.
+verifs.calculateSig()
+
+#If there are params...
+if paramCount() > 0:
+    publicKey = newBLSPublicKey(paramStr(1))
+else:
+    #Else, create a new wallet.
+    var miner: MinerWallet = newMinerWallet()
+    publicKey = miner.publicKey
+    echo "No wallet was passed in. A new one has been created with a Private Key of " & $miner.privateKey & "."
+
+miners = @[(
+    newMinerObj(
+        publicKey,
+        100
+    )
+)]
+
+#Acquire the RPC.
+proc acquireRPC() {.async.} =
+    #Make sure no other async procs are using the RPC.
+    while rpcBool:
+        #If they are, sleep so they can finish.
+        await sleepAsync(1)
+    rpcBool = true
+    #Make sure no other threads are using the RPC.
+    acquire(rpcLock)
+
+#Release the RPC.
+proc releaseRPC() =
+    rpcBool = false
+    release(rpcLock)
+
+#Check for Verifications.
+proc checkup() {.async.} =
+    while true:
+        #Run every ten seconds.
+        await sleepAsync(10000)
+
+        #Get the Verifications.
+        await acquireRPC()
+        var jsonVerifs: JSONNode = await rpc.lattice.getUnarchivedVerifications()
+        releaseRPC()
+
+#Main function is so this runs async.
 proc main() {.async.} =
-    var
-        #Connect to the EMB Node.
-        rpc: EmberRPC = await newEmberRPC()
-        #Create a Wallet for signing Verifications.
-        miner: MinerWallet
+    #Block.
+    var newBlock: Block
 
-    #If there are params...
-    if paramCount() > 0:
-        #If a key was passed...
-        miner = newMinerWallet(newBLSPrivateKeyFromBytes(paramStr(1)))
-    else:
-        #Else, create a new wallet.
-        miner = newMinerWallet()
-        echo "No wallet was passed in. A new one has been created with a Private Key of " & $miner.privateKey & "."
-
-    var
-        #Difficulty.
-        difficulty: BN
-        #Gensis string.
-        genesis: string = "mainnet"
-        #Block.
-        newBlock: Block
-        #Nonce.
-        nonce: uint = 1
-        #Last Block hash.
-        last: ArgonHash = (
-            await rpc.merit.getBlock(
-                (await rpc.merit.getHeight()) - 1
-            )
-        )["argon"].getStr().toArgonHash()
-        #Verifications object.
-        verifs: Verifications = newVerificationsObj()
-        #Miners object.
-        miners: Miners = @[(
-            newMinerObj(
-                miner.publicKey,
-                100
-            )
-        )]
-    #Calculate the Verifications' signature.
-    verifs.calculateSig()
+    #Start the checkup proc.
+    asyncCheck checkup()
 
     #Mine the chain.
     while true:
         #Get the difficulty.
+        await acquireRPC()
         difficulty = newBN(await rpc.merit.getDifficulty())
+        releaseRPC()
 
         #Create a block.
         newBlock = newBlock(
@@ -101,7 +142,9 @@ proc main() {.async.} =
 
                 #Publish the block.
                 try:
+                    await acquireRPC()
                     await rpc.merit.publishBlock(newBlock.serialize())
+                    releaseRPC()
                 except:
                     echo "The miner submitted a Block the Node considered invalid."
                     quit(-1)
